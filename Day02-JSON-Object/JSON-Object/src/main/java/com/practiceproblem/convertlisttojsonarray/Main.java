09face8e5d6952dd1326fdeddadb1a24423e19c7
Raksha <rak070803@gmail.com>
2025-02-20 16:08:45 +0530

    [Raksha] add.  convert json object to array

---
+++ Day02-JSON-Object/JSON-Object/src/main/java/com/practiceproblem/convertlisttojsonarray/Main.java
@@ -0,0 +1,28 @@
+package com.practiceproblem.convertlisttojsonarray;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ForwardingIterator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            List<Car> cars= new ArrayList<>();
+            cars.add(new Car("BMW", "M5", 2024));
+            cars.add(new Car("Audi", "A6", 2023));
+            cars.add(new Car("Tesla", "Model S", 2022));
+            ObjectMapper objectMapper= new ObjectMapper();
+            String jsonArray = objectMapper.writeValueAsString(cars);
+
+            objectMapper.writeValue(new File("src/main/resources/objectList.json"), cars);
+
+            System.out.println(jsonArray);
+            } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        }
+    }
+
