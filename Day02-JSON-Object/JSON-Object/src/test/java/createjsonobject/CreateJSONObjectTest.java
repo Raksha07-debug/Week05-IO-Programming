6c440716409eae82d997d46fec38c06ea50de44c
Raksha <rak070803@gmail.com>
2025-02-20 10:41:34 +0530

    [Raksha] add.  Create JSOn object

---
+++ Day02-JSON-Object/JSON-Object/src/test/java/createjsonobject/CreateJSONObjectTest.java
@@ -0,0 +1,15 @@
+package createjsonobject;
+
+
+import com.practiceproblem.CreateJsonObject;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateJSONObjectTest {
+
+    @Test
+    void testJsonObjectCreation() {
+        String ans="{\"subject\":[\"Java\",\"Hindi\",\"Science\"],\"name\":\"Raksha\",\"age\":21}";
+        assertEquals(CreateJsonObject.createJsonObject(),ans);
+    }
+}
